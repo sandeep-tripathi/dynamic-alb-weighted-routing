c1e98ea51368269b86ef2013f2dfb5cd469d8397
Sandeep Tripathi <39844255+sandeep-tripathi@users.noreply.github.com>
2024-08-25 17:39:39 +0200

    aws_auth.py

---
+++ aws_auth.py
@@ -0,0 +1,9 @@
+import os
+import boto3
+from typing import Any
+# Static authentication is bad practice is discouraged
+from typing import Any
+
+#def get_ec2_client() -> Any:
+    """Initialize and return a boto3 client for the EC2 service."""
+#    return boto3.client('ec2')
